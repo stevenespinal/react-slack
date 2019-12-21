d7a621972e9ea4936bcb3e80c057731062cd3ad2
Steven Espinal <stevenjesusespinal@gmail.com>
2019-12-20 23:34:35 -0500

    added mock up components and side bar

---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Menu} from "semantic-ui-react";
+
+class SidePanel extends Component {
+  render() {
+    return (
+      <Menu size="large" inverted fixed="left" vertical style={{background: "#4c3c4c", fontSize:"1.2rem"}}>
+
+      </Menu>
+    )
+  }
+}
+
+export default SidePanel;
